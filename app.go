05195616d484b9724dcceffe18799c478232221f
Jeormy0515 <qkr07224@gmail.com>
2022-03-04 14:44:50 +0900

    CI/CD Automation Test

--- app.go
+++ app.go
@@ -10,7 +10,7 @@ func main() {
 	})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.SendString("{ \"status\" : \"ok\" }")
+		return c.SendString("{ \"status\" : \"up\" }")
 	})
 
 	app.Listen("0.0.0.0:3000")
